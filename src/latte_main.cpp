69a115b1467b89d910deba8067c1098f6025b5a1
Jakub Paczek <jakub.dawid.paczek@gmail.com>
2026-01-18 17:18:09 +0100

    move semantic analysis files to separate folder

--- src/latte_main.cpp
+++ src/latte_main.cpp
@@ -7,8 +7,8 @@
 #include "frontend/Parser.H"
 #include "frontend/Absyn.H"
 
-#include "typecheck.hpp"
-#include "latte_error.hpp"
+#include "semantic/typecheck.hpp"
+#include "semantic/latte_error.hpp"
 
 #include "backend/ir.hpp"
 #include "backend/regalloc.hpp"
